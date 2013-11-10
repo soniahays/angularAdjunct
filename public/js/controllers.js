a104360e7590e52cd7c22e950036263c2541080e
Sonia Brahmi <7sonia@gmail.com>
2013-11-10 00:24:23 -0500

    pop-over

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -31,7 +31,7 @@ angular.module('myApp.controllers', ['$strap.directives'])
 //
 //    }])
 
-    .controller('EditableFormCtrl',['$scope','$rootScope','$http', function ($scope,$rootScope,$http) {
+    .controller('EditableFormCtrl',['$scope','$rootScope','$filter','$http', function ($scope,$rootScope,$filter,$http) {
         console.log($rootScope.user);
         $scope.user = $rootScope.user;
         $scope.user={
@@ -41,8 +41,30 @@ angular.module('myApp.controllers', ['$strap.directives'])
             institution:'Michigan State University',
             city:'East Lansing',
             generalArea:'Michigan',
-            department:'History Dpt.'
+            department:'History Dpt.',
+            specialty1: '20th Century',
+            specialty2:'Women & Gender',
+            specialty3:'Europe & Russia',
+            educationLevel:'Ph.D (expected)',
+            summary:'Jennifer is currently pursuing her graduate degree at Michigan State University. Her research interests include Poland, the Holocaust, European Jewry Gender Childhood and Family. She has over six years of experience as an instructor and teaching assistant. Jennifer is a tech savvy teacher and has been enhancing her classes with Youtube video and online questionnaire for four years now ',
+            experience1Institution:'Saginaw Valley State University',
+            experience1Title:'Instructor',
+            experience1Location:'fall 2013, Kochville, Michigan ',
+            status:1,
+            experience1TimePeriodYear:'2013',
+            experience1Summary:'write more about your experience here'
         };
+        $scope.statuses = [
+            {value:1, text:'fall'},
+            {value:2, text:'winter'},
+            {value:3, text:'spring'},
+            {value:4, text:'summer'}
+        ];
+        $scope.saveUser = function() {
+            //$scope.user already updated!
+            return $http.post('/saveUser', $scope.user);
+        };
+
     }])
 
     .controller('ConfirmEmailCtrl' ['$scope','$location',   function ($scope,$location) {
