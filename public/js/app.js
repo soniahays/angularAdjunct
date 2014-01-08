98e6d6398f1eac7474287c5df06336c595e60eee
Nader Chehab <naderchehab@gmail.com>
2014-01-08 00:17:58 -0500

    added ngScrollTo

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@
 angular.module('adjunct', [
         'ngRoute',
         'monospaced.elastic',
-        'scrollto',
+        'ngScrollTo',
         'ngCookies',
         //'ngResource',
         '$strap.directives',
