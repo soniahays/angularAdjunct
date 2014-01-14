53d65db5ba8da0ac84280b11d1d3a0e84582ac3b
Sonia Brami <7sonia@gmail.com>
2014-01-13 19:55:30 -0500

    signup form password validation -in progress

--- public/js/controllers/signup.js
+++ public/js/controllers/signup.js
@@ -2,7 +2,7 @@
 
 angular.module('adjunct.controllers')
     .controller('SignupCtrl', ['$scope', '$rootScope', '$http', '$location', function ($scope, $rootScope, $http, $location) {
-
+        $scope.pw='';
         $scope.joinNow = function () {
             $scope.user.email = encodeURIComponent($scope.user.email);
             $rootScope.user = $scope.user;
@@ -10,7 +10,7 @@ angular.module('adjunct.controllers')
         }
 
         $scope.tooltip = {
-            "title": "Password must meet the following requirements:</br><div class='pwdHasLetter' style='color:blue'>At least <strong>one letter</strong></div></br>At least <strong>one number</strong></br>At least <strong>8 characters long</strong>",
+            "title": "<div class='input-help'>Password must meet the following requirements:</br><ul><li ng-class='pwdHasLetter'>At least <strong>one letter</strong></li><li ng-class='pwdHasNumber'>At least <strong>one number</strong></li><li ng-class='pwdValidLength'>At least <strong>8 characters long</strong></li></ul></div>",
             "checked": false
         };
     }]);
