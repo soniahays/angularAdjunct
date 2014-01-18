d524d75eaabf55afb13f3a25e3590aa11407abb9
Sonia Brami <7sonia@gmail.com>
2014-01-17 20:19:15 -0500

    clean-up

--- public/js/controllers/signup.js
+++ public/js/controllers/signup.js
@@ -17,6 +17,14 @@ angular.module('adjunct.controllers')
             "title": "<ul id='strength' check-strength='password'></ul>",
             "checked": false
         };
+
+        $scope.importLinkedin = function () {
+            $scope.user.email = encodeURIComponent($scope.user.email);
+            $scope.user.password = $scope.password;
+            $rootScope.user = $scope.user;
+            $http.post('/api/signup', JSON.stringify({'user': $scope.user})).then(function(){$location.path('/basic-profile');});
+            window.location.replace(window.location.origin + "/api/linkedInAuth");
+        }
     }]);
 
 
