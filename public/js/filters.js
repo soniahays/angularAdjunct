7c8b873ba3f54e9ce11e3b6250e570892a307458
Nader Chehab <naderchehab@gmail.com>
2014-02-05 23:38:51 -0500

    added line breaks in profile

--- public/js/filters.js
+++ public/js/filters.js
@@ -7,4 +7,10 @@ angular.module('adjunct.filters').
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     }
-  }]);
+  }]).filter('newlines', function () {
+    return function(text) {
+        if(text)
+            return text.replace(/\n/g, '<br/>');
+        return '';
+    }
+});
