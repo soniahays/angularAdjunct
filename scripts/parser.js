5f36371b6841d7908698e4a7e79f656b9a429f31
Nader Chehab <naderchehab@gmail.com>
2014-04-05 12:27:47 -0400

    Merge branch 'master' of bitbucket.org:sonia_brami/angular-adjunct
    Conflicts:
    	public/dist/adjuncts.js
    	public/dist/adjuncts.min.js

--- scripts/parser.js
+++ scripts/parser.js
@@ -15,35 +15,40 @@ MongoClient.connect("mongodb://localhost:27017/adjunct", function (err_, db) {
     fs.readdir(path, function(err, files) {
 
         if (err) {
-            console.error(err);
+            console.error("Error reading dir", err);
             return;
         }
+
+        var count = -1;
 
         for (var i = 0; i < files.length; i++) {
 
             fs.readFile(path + files[i], 'utf8', function (err,data) {
+
                 if (err) {
-                    return console.log(err);
+                    return console.log("Error reading file", err);
                 }
+
                 var $ = cheerio.load(data);
                 var jobTitle = getField($, "Job Title");
                 var jobNumber = getField($, "Job Number");
                 var datePosted = getField($, "Date Posted");
                 var applicationDeadline = getField($, "Application Deadline");
                 var jobDescription = getDescription($, "Job Description");
+                var contact = getField($, "Contact");
                 console.log("jobTitle", jobTitle);
                 console.log("jobNumber", jobNumber);
                 console.log("datePosted", datePosted);
                 console.log("applicationDeadline", applicationDeadline);
                 console.log("jobDescription", jobDescription);
+                console.log("contact", contact);
 
                 if (jobTitle != "" && datePosted != "" && jobDescription != "") {
-                    insertJob({jobTitle: jobTitle, datePosted: datePosted, applicationDeadline: applicationDeadline, jobDescription: jobDescription});
+                    insertJob({jobTitle: jobTitle, contact: contact, datePosted: datePosted, applicationDeadline: applicationDeadline, jobDescription: jobDescription});
                 }
             });
         }
     });
-
 
     function getField($, str) {
         return $("font:contains(" + str + ":)").parent().parent().next().text().trim();
@@ -57,14 +62,12 @@ MongoClient.connect("mongodb://localhost:27017/adjunct", function (err_, db) {
         var collection = db.collection('jobs');
         collection.insert([job], function (err) {
             if (err) {
-                return console.error(err);
+                return console.error("Error inserting in db", err);
             }
             else {
                 return console.log("done");
             }
         });
     }
-
-
 });
 
